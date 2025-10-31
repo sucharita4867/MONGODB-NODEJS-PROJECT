7fd76d6cd88332acad2db3d3112adea0097f29e1
sucharita4867 <sucharitasardar7@gmail.com>
2025-10-31 23:50:38 +0530

    added practice

--- smart-deals-server/index.js
+++ smart-deals-server/index.js
@@ -31,7 +31,14 @@ async function run() {
     const productsCollection = db.collection("products");
 
     app.get("/products", async (req, res) => {
-      const cursor = productsCollection.find().sort({ price_min: 1 });
+      // const projectFields = { title: 1, price_min: 1, price_max: 1, image: 1 };
+      // const cursor = productsCollection
+      //   .find()
+      //   .sort({ price_min: 1 })
+      //   .skip(4)
+      //   .limit(3)
+      //   .project(projectFields);
+      const cursor = productsCollection.find();
       const result = await cursor.toArray();
       res.send(result);
     });
