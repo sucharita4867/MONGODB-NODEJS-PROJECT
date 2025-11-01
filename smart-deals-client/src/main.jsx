51d0b6e878a3f74de97364f514f0d1ba7f3579e7
sucharita4867 <sucharitasardar7@gmail.com>
2025-11-01 16:22:54 +0530

    added router setup and components or layout

--- smart-deals-client/src/main.jsx
+++ smart-deals-client/src/main.jsx
@@ -1,10 +1,32 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import "./index.css";
+import App from "./App.jsx";
+import { createBrowserRouter } from "react-router";
+import { RouterProvider } from "react-router/dom";
+import RootLayout from "./Layout/RootLayout.jsx";
+import Home from "./Components/Home/Home.jsx";
+import AllProducts from "./Components/AllProducts/AllProducts.jsx";
 
-createRoot(document.getElementById('root')).render(
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <RootLayout />,
+    children: [
+      {
+        path: "/",
+        element: <Home />,
+      },
+      {
+        path: '/allProducts',
+        element: <AllProducts/>
+      }
+    ],
+  },
+]);
+
+createRoot(document.getElementById("root")).render(
   <StrictMode>
-    <App />
-  </StrictMode>,
-)
+    <RouterProvider router={router} />,
+  </StrictMode>
+);
