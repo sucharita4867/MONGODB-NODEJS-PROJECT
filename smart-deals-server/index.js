eb598fde126360153ca1fb06bc8c893d997e1eeb
sucharita4867 <sucharitasardar7@gmail.com>
2025-11-02 07:54:21 +0530

    added this task

--- smart-deals-server/index.js
+++ smart-deals-server/index.js
@@ -29,8 +29,21 @@ async function run() {
 
     const db = client.db("smart_db");
     const productsCollection = db.collection("products");
-
     const bidsCollection = db.collection("bids");
+    const usersCollection = db.collection("users");
+
+    app.post("/users", async (req, res) => {
+      const newUser = req.body;
+      const email = req.body.email;
+      const query = { email: email };
+      const existingUser = await usersCollection.findOne(query);
+      if (existingUser) {
+        res.send({ massage: "user already exits.do not need to insert again" });
+      } else {
+        const result = await usersCollection.insertOne(newUser);
+        res.send(result);
+      }
+    });
 
     app.get("/products", async (req, res) => {
       // const projectFields = { title: 1, price_min: 1, price_max: 1, image: 1 };
@@ -87,9 +100,28 @@ async function run() {
       res.send(result);
     });
     // bids related apis
+
+    app.delete("/bids/:id", async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: new ObjectId(id) };
+      const result = await bidsCollection.deleteOne(query);
+      res.send(result);
+    });
+
     app.get("/bids", async (req, res) => {
-      const cursor = bidsCollection.find();
+      const email = req.query.email;
+      const query = {};
+      if (email) {
+        query.buyer_email = email;
+      }
+      const cursor = bidsCollection.find(query);
       const result = await cursor.toArray();
+      res.send(result);
+    });
+
+    app.post("/bids", async (req, res) => {
+      const newBid = req.body;
+      const result = await bidsCollection.insertOne(newBid);
       res.send(result);
     });
 
