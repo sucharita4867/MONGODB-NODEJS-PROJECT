313cad75269f4c5fed5321eaa442483e661cefd3
sucharita4867 <sucharitasardar7@gmail.com>
2025-11-04 23:53:33 +0530

    delete all text in products details page lorem

--- smart-deals-client/src/Components/ProductsDetails/ProductsDetails.jsx
+++ smart-deals-client/src/Components/ProductsDetails/ProductsDetails.jsx
@@ -4,15 +4,7 @@ import { useLoaderData } from "react-router";
 const ProductsDetails = () => {
   const product = useLoaderData();
   console.log(product);
-  return (
-    <div>
-      <h3>
-        Lorem ipsum dolor sit amet consectetur adipisicing elit. Cum quas,
-        mollitia eum modi vel ipsa officia, autem optio natus quam unde numquam
-        beatae, corporis ipsam quos laboriosam distinctio atque? Consequuntur?
-      </h3>
-    </div>
-  );
+  return <div></div>;
 };
 
 export default ProductsDetails;
