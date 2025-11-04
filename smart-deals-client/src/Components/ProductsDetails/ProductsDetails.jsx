2f9bb4971d9cc0dcfeac2c9ebf8ac805ae759f4d
sucharita4867 <sucharitasardar7@gmail.com>
2025-11-04 23:51:08 +0530

    added products details dpage h3

--- smart-deals-client/src/Components/ProductsDetails/ProductsDetails.jsx
+++ smart-deals-client/src/Components/ProductsDetails/ProductsDetails.jsx
@@ -10,6 +10,9 @@ const ProductsDetails = () => {
         Lorem ipsum dolor sit amet consectetur adipisicing elit. Cumque quae
         soluta nemo.
       </h1>
+      <h3>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Cum quas, mollitia eum modi vel ipsa officia, autem optio natus quam unde numquam beatae, corporis ipsam quos laboriosam distinctio atque? Consequuntur?
+      </h3>
     </div>
   );
 };
