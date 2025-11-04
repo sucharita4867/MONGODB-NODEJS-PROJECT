f905bfe3e0256a9d5a5c5090f571c7e18a0dc253
sucharita4867 <sucharitasardar7@gmail.com>
2025-11-04 23:52:35 +0530

    delete dproducts page h2 lorem

--- smart-deals-client/src/Components/ProductsDetails/ProductsDetails.jsx
+++ smart-deals-client/src/Components/ProductsDetails/ProductsDetails.jsx
@@ -6,12 +6,10 @@ const ProductsDetails = () => {
   console.log(product);
   return (
     <div>
-      <h1>
-        Lorem ipsum dolor sit amet consectetur adipisicing elit. Cumque quae
-        soluta nemo.
-      </h1>
       <h3>
-        Lorem ipsum dolor sit amet consectetur adipisicing elit. Cum quas, mollitia eum modi vel ipsa officia, autem optio natus quam unde numquam beatae, corporis ipsam quos laboriosam distinctio atque? Consequuntur?
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Cum quas,
+        mollitia eum modi vel ipsa officia, autem optio natus quam unde numquam
+        beatae, corporis ipsam quos laboriosam distinctio atque? Consequuntur?
       </h3>
     </div>
   );
