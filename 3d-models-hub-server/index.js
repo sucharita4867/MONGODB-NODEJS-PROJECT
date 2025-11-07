3a4ba44e0ac3beb440765b56e0cad38b0cbfc009
sucharita4867 <sucharitasardar7@gmail.com>
2025-11-07 17:16:07 +0530

    added modles show in ui

---
+++ 3d-models-hub-server/index.js
@@ -0,0 +1,51 @@
+const express = require("express");
+const app = express();
+const { MongoClient, ServerApiVersion } = require("mongodb");
+const cors = require("cors");
+const port = 3000;
+// user:practiceDB
+// pass:FRckubLeEvd9EDTL
+app.use(cors());
+app.use(express.json());
+
+const uri =
+  "mongodb+srv://practiceDB:FRckubLeEvd9EDTL@cluster0.phhktud.mongodb.net/?appName=Cluster0";
+
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  },
+});
+
+async function run() {
+  try {
+    await client.connect();
+
+    const db = client.db("model-db");
+    const modelCollection = db.collection("models");
+
+    app.get("/models", async (req, res) => {
+      const result = await modelCollection.find().toArray();
+      res.send(result);
+    });
+
+    await client.db("admin").command({ ping: 1 });
+    console.log(
+      "Pinged your deployment. You successfully connected to MongoDB!"
+    );
+  } finally {
+    // Ensures that the client will close when you finish/error
+    //     await client.close();
+  }
+}
+run().catch(console.dir);
+
+app.get("/", (req, res) => {
+  res.send("Hello World!");
+});
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`);
+});
